a96da9c4f64bcc10fbec0e4cc91c4af3090b591d
DNovotny1 <DNovotny1@users.noreply.github.com>
2018-01-29 20:49:26 -0600

    broken
    broke exit sequence

---
+++ Calc.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	bool contin = true;
+	char sign;
+	char quit;
+	while (contin)
+	{
+		cout << "Please enter your first number. ";
+		cin >> quit;
+		if(quit == y) || (quit==Y)
+		{
+			contin = true;
+		}
+	}
+	
+	
+	
+	return 0;
+}
